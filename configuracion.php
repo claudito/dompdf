5a09754f72b5f6c6ed13ce7fb776f4c6c5e0f436
claudito <megabyte1507@gmail.com>
2016-06-30 14:29:05 -0500

    update 30/06/2016

---
+++ configuracion.php
@@ -0,0 +1,14 @@
+<?php 
+ date_default_timezone_set('America/Lima');
+define("PATH", "dompdf");
+define("SERVERBD", "localhost");
+define("USERBD", "root");
+define("PASSBD", "");
+define("BD", "clase01");
+
+
+
+
+
+
+ ?>
--- reporte/alumno.php
+++ reporte/alumno.php
@@ -1,10 +1,10 @@
 <?php 
 # Cargamos la librería dompdf.
-require_once '../dompdf_config.inc.php';
+include('../librerias/dompdf/dompdf_config.inc.php');
  
 # Contenido HTML del documento que queremos generar en PDF.
 ob_start();
-include 'reporte.php';
+include('../consulta/alumno.php');
  
  $html=ob_get_clean();
 # Instanciamos un objeto de la clase DOMPDF.
@@ -13,18 +13,18 @@ $mipdf = new DOMPDF();
 # Definimos el tamaño y orientación del papel que queremos.
 # O por defecto cogerá el que está en el fichero de configuración.
  #ini_set("memory_limit","32M");   
-$mipdf ->set_paper('a4','landscape'); #horizontal
-//$mipdf ->set_paper('a4','letter'); #vertical
+//$mipdf ->set_paper('a4','landscape'); #horizontal
+$mipdf ->set_paper('a4','letter'); #vertical
  
 # Cargamos el contenido HTML.
-$mipdf ->load_html(utf8_decode($html));
+$mipdf ->load_html($html);
  
 # Renderizamos el documento PDF.
 $mipdf ->render();
  
 # Enviamos el fichero PDF al navegador.
-//$mipdf ->stream('LISTA DE EMPLEADOS.pdf');#Descargar Pdf
-$mipdf ->stream('LISTA DE EMPLEADOS.pdf',array('Attachment'=>0));#Previzualizar
+//$mipdf ->stream('LISTA DE ALUMNOS.pdf');#Descargar Pdf
+$mipdf ->stream('LISTA DE ALUMNOS.pdf',array('Attachment'=>0));#Previzualizar
 
 
 ?>
